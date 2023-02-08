f183d410e9684e7a6bc5cc00b7f71f3d00a28d66
Graham Waters <graham.waters.business@gmail.com>
2023-02-08 13:46:10 -0600

    sorted and reorg

--- full repo/empty_project_example.py
+++ full repo/empty_project_example.py
@@ -56,7 +56,53 @@ import cv2, imageio, matplotlib.pyplot as plt, numpy as np, PIL, PIL.Image as Im
 
 # ─── The Prompt ───────────────────────────────────────────────────────────────
 """
-
-
-
+Instructions for GitHub Copilot:
+    Write a function that reads an XLSX file and returns dataframes where each sheet is a dataframe, each column of each df should be type-cast based on the values from the corresponding sheet in the second row, and the column names are in the first row. The function should save these dataframes in a folder called `extracted_dfs` with each sheet in its own folder named after the sheet's name in the file. Within these folders, the function should save the dataframe as a pickle file. The function should return a dictionary with the sheet names as keys and the dataframe information as values.
 """
+
+# ─── The Code ─────────────────────────────────────────────────────────────────
+def read_xlsx(file_path):
+    # Read in the file
+    with pd.ExcelFile(file_path) as xlsx:
+        # Get the sheet names
+        sheet_names = xlsx.sheet_names
+
+        # Create an empty dictionary to store the dataframes
+        dfs = {}
+
+        # Iterate through the sheets
+        for sheet in sheet_names:
+            # Get the dataframe from the sheet
+            df = pd.read_excel(xlsx, sheet_name=sheet)
+
+            # Get the second row of the dataframe
+            second_row = df.iloc[1]
+
+            # Get the first row of the dataframe
+            first_row = df.iloc[0]
+
+            # Get the column names from the first row
+            df.columns = first_row
+
+            # Drop the first two rows
+            df = df.iloc[2:]
+
+            # Iterate through the columns in the second row
+            for col in second_row.index:
+                # Get the type of the value in the second row
+                col_type = type(second_row[col])
+
+                # Cast the column to the type of the second row value
+                df[col] = df[col].astype(col_type)
+
+            # Create a folder for the sheet
+            os.makedirs(f"extracted_dfs/{sheet}", exist_ok=True)
+
+            # Save the dataframe as a pickle file in the sheet folder
+            df.to_pickle(f"extracted_dfs/{sheet}/{sheet}.pkl")
+
+            # Add the dataframe to the dictionary
+            dfs[sheet] = df
+
+    # Return the dictionary
+    return dfs
