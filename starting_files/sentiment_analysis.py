d3ced2d78e9a9835224027a70b6e3a39c9aa1312
Graham Waters <graham.waters.business@gmail.com>
2023-02-08 16:41:41 -0600

    main: Updated

--- starting_files/sentiment_analysis.py
+++ starting_files/sentiment_analysis.py
@@ -1,5 +1,6 @@
 
 # use sentiment analysis in VADER to analyze the sentiment of each chapter of Great Expectations by Charles Dickens. Use the Gutenberg Project API to get the text of the book. Use the nltk library to tokenize the text into sentences. Use the VADER sentiment analyzer to get the sentiment of each sentence. Use the seaborn library to plot the sentiment of each sentence as a swarm plot. Use the matplotlib library to add a title to the plot. Use the matplotlib library to save the plot as a png file.
+# to optimize this for the time we have, let's just do the first chapter
 
 import requests
 import nltk
@@ -14,6 +15,12 @@ def get_book():
     get_book Use the Gutenberg Project API to get the text of the book.
     """
     book = requests.get('http://www.gutenberg.org/files/1400/1400-0.txt').text
+    # just get the first chapter for now which is the second occurrence of 'Chapter I' to the second occurrence of 'Chapter II'
+    # second occurrence of 'Chapter I' is second element of find all 'Chapter I'
+    start = book.find_all('Chapter I')[1]
+    end = book.find_all('Chapter II')[1]
+    book = book[start:end]
+    print(book)
     return book
 
 def tokenize(book):
@@ -39,14 +46,7 @@ def swarm_plot(sentiment_scores):
     swarm_plot Use the seaborn library to plot the sentiment of each sentence as a swarm plot.
     """
     df = pd.DataFrame(sentiment_scores)
-    plt.figure(figsize=(10, 5))
     sns.swarmplot(x='compound', y='sentence', data=df)
-    plt.title('Sentiment of each sentence in Great Expectations')
-    plt.xlabel('Sentiment Score')
-    plt.ylabel('Sentence')
-    plt.legend(loc='upper right')
-    plt.savefig('sentiment.png')
-    plt.show()
 
 def add_title():
     """
